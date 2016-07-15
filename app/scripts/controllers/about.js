876c890e908aec783afcc9c47e66e33493e1bc23
Eduardo Jimenez <worldjiz@hotmail.com>
2016-07-15 14:28:26 -0500

    Inicio de proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appDomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appDomApp
+ */
+angular.module('appDomApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
